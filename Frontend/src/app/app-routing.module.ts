7767271c7158e55bec9ac37b6bec70f05cae598e
MAP Piblo <muhmuhammad@apps.ipb.ac.id>
2026-05-13 21:56:26 +0700

    chore: upgrade Angular 14 to 15

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -132,7 +132,7 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top', relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
